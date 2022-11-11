c240f73a83b8925f1bc354c8706b44f0dc503cda
yutzuochen <yutzuochen@gmail.com>
2022-11-12 02:12:50 +0800

    [update]1.add login option at blog page 2.add option at dashboard page

--- account/urls.py
+++ account/urls.py
@@ -4,11 +4,9 @@ from . import views
 
 urlpatterns = [
     # previous login view
-    # path('login/', views.user_login, name='login'),
-
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+   
     path('', views.dashboard, name='dashboard'), # dashboard 需要 login 成功
+    #path('', include('django.contrib.auth.urls')),
     # view by self
     path('login/', auth_views.LoginView.as_view(), name='login'),
     #path('login/', views.user_login, name='login'),
